ef9b92df6be261b49b7a049d13af9e964c312ce6
Hazael Ventura <1234123josue@gmail.com>
2025-10-17 14:37:59 -0600

    17 de Octubre 2025

---
+++ MainUser.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class MainUser {
+    public static void main(String[] args) {
+        Scanner sca = new Scanner (System.in);
+        System.out.println("Dame el nombre: ");
+        String nombre = sca.nextLine();
+        System.out.println("Dame la edad: ");
+        int edad = sca.nextInt();
+        sca.nextLine();
+        System.out.println("Dame el correo: ");
+        String correo = sca.nextLine();
+
+        Usuario u1 = new Usuario(nombre, correo, edad);
+        u1.setNombre(nombre);
+        u1.setEdad(edad);
+        u1.setCorreo(correo);
+        System.out.println("**USUARIO**\n"+ "Nombre :"+u1.getNombre()+"\nEdad:"+u1.getEdad()+"\nCorreo:"+u1.getCorreo());
+    }
+}
---
+++ Usuario.java
@@ -0,0 +1,28 @@
+class Usuario{
+    String nombre, correo;
+    int edad;
+    Usuario (String nombre, String correo, int edad){
+        this.nombre=nombre;
+        this.correo=correo;
+        this.edad=edad;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getCorreo() {
+        return correo;
+    }
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+    public int getEdad() {
+        return edad;
+    }
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+}
